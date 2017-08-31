e57f88947c3589bde197366ac4716bac9ef7ac74
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-08-31 08:53:02 -0600

    all data massage written. Need SetInterval().

--- getData.js
+++ getData.js
@@ -19,150 +19,148 @@ mongoose.connect('mongodb://localhost:27017/IBNChallenge', {
   console.log("Database Connected");
 })
 
-// fetchUrl('https://www.coincap.io/page/ETH', function(error, meta, result){
-//   let json = JSON.parse(result)
-//
-//   let newCoinCap = new CoinCap({
-//     coin: json.display_name,
-//     coin_id: json.id,
-//     market_cap: json.market_cap,
-//     price_usd: json.price_usd,
-//     price_btc: json.price_btc,
-//     supply: json.supply,
-//     total_cap: json.total_cap,
-//     volume: json.volume,
-//     timestamps: Date.now()
-//   })
-//
-//   newCoinCap.save().then(function(result){
-//     console.log('Successful New CoinCap.', result);
-//   }).catch(function(err){
-//     console.log('Error. CoinCap Not Created.', err);
-//   })
-// })
-//
-// fetchUrl('https://www.coincap.io/page/LTC', function(error, meta, result){
-//   let json = JSON.parse(result)
-//
-//   let newCoinCap = new CoinCap({
-//     coin: json.display_name,
-//     coin_id: json.id,
-//     market_cap: json.market_cap,
-//     price_usd: json.price_usd,
-//     price_btc: json.price_btc,
-//     supply: json.supply,
-//     total_cap: json.total_cap,
-//     volume: json.volume,
-//     timestamps: Date.now()
-//   })
-//
-//   newCoinCap.save().then(function(result){
-//     console.log('Successful New CoinCap.', result);
-//   }).catch(function(err){
-//     console.log('Error. CoinCap Not Created.', err);
-//   })
-// })
-//
-// fetchUrl('https://www.coincap.io/page/DASH', function(error, meta, result){
-//   let json = JSON.parse(result)
-//
-//   let newCoinCap = new CoinCap({
-//     coin: json.display_name,
-//     coin_id: json.id,
-//     market_cap: json.market_cap,
-//     price_usd: json.price_usd,
-//     price_btc: json.price_btc,
-//     supply: json.supply,
-//     total_cap: json.total_cap,
-//     volume: json.volume,
-//     timestamps: Date.now()
-//   })
-//
-//   newCoinCap.save().then(function(result){
-//     console.log('Successful New CoinCap.', result);
-//   }).catch(function(err){
-//     console.log('Error. CoinCap Not Created.', err);
-//   })
-// })
+fetchUrl('https://www.coincap.io/page/ETH', function(error, meta, result){
+  let json = JSON.parse(result)
 
-// fetchUrl('https://poloniex.com/public?command=returnTicker', function(error, meta, result){
-//   let json = JSON.parse(result)
-//   let LTC = json.BTC_LTC
-//
-//   let newLTC = new Poloniex({
-//     coin: "LTC",
-//     pair_name: "BTC_LTC",
-//     baseVolume: LTC.baseVolume,
-//     high24Hr: LTC.high24Hr,
-//     highestBid: LTC.highestBid,
-//     lastTradePrice: LTC.last,
-//     low24Hr: LTC.low24Hr,
-//     lowestAsk: LTC.lowestAsk,
-//     percentChange: LTC.percentChange,
-//     quoteVolume: LTC.quoteVolume,
-//     timestamps: Date.now()
-//   })
-//
-//   newLTC.save().then(function(result){
-//     console.log('Successful New Poloniex.', result);
-//   }).catch(function(err){
-//     console.log('Error. Poloniex Not Created.', err);
-//   })
-//
-//
-//   let ETH = json.BTC_ETH
-//
-//   let newETH = new Poloniex({
-//     coin: "ETH",
-//     pair_name: "BTC_ETH",
-//     baseVolume: ETH.baseVolume,
-//     high24Hr: ETH.high24Hr,
-//     highestBid: ETH.highestBid,
-//     lastTradePrice: ETH.last,
-//     low24Hr: ETH.low24Hr,
-//     lowestAsk: ETH.lowestAsk,
-//     percentChange: ETH.percentChange,
-//     quoteVolume: ETH.quoteVolume,
-//     timestamps: Date.now()
-//   })
-//
-//   newETH.save().then(function(result){
-//     console.log('Successful New Poloniex.', result);
-//   }).catch(function(err){
-//     console.log('Error. Poloniex Not Created.', err);
-//   })
-//
-//
-//   let DASH = json.BTC_DASH
-//
-//   let newDASH = new Poloniex({
-//     coin: "DASH",
-//     pair_name: "BTC_DASH",
-//     baseVolume: DASH.baseVolume,
-//     high24Hr: DASH.high24Hr,
-//     highestBid: DASH.highestBid,
-//     lastTradePrice: DASH.last,
-//     low24Hr: DASH.low24Hr,
-//     lowestAsk: DASH.lowestAsk,
-//     percentChange: DASH.percentChange,
-//     quoteVolume: DASH.quoteVolume,
-//     timestamps: Date.now()
-//   })
-//
-//
-//   newDASH.save().then(function(result){
-//     console.log('Successful New Poloniex.', result);
-//   }).catch(function(err){
-//     console.log('Error. Poloniex Not Created.', err);
-//   })
-// })
+  let newCoinCap = new CoinCap({
+    coin: json.display_name,
+    coin_id: json.id,
+    market_cap: json.market_cap,
+    price_usd: json.price_usd,
+    price_btc: json.price_btc,
+    supply: json.supply,
+    total_cap: json.total_cap,
+    volume: json.volume,
+    timestamps: Date.now()
+  })
+
+  newCoinCap.save().then(function(result){
+    console.log('Successful New CoinCap.', result);
+  }).catch(function(err){
+    console.log('Error. CoinCap Not Created.', err);
+  })
+})
+
+fetchUrl('https://www.coincap.io/page/LTC', function(error, meta, result){
+  let json = JSON.parse(result)
+
+  let newCoinCap = new CoinCap({
+    coin: json.display_name,
+    coin_id: json.id,
+    market_cap: json.market_cap,
+    price_usd: json.price_usd,
+    price_btc: json.price_btc,
+    supply: json.supply,
+    total_cap: json.total_cap,
+    volume: json.volume,
+    timestamps: Date.now()
+  })
+
+  newCoinCap.save().then(function(result){
+    console.log('Successful New CoinCap.', result);
+  }).catch(function(err){
+    console.log('Error. CoinCap Not Created.', err);
+  })
+})
+
+fetchUrl('https://www.coincap.io/page/DASH', function(error, meta, result){
+  let json = JSON.parse(result)
+
+  let newCoinCap = new CoinCap({
+    coin: json.display_name,
+    coin_id: json.id,
+    market_cap: json.market_cap,
+    price_usd: json.price_usd,
+    price_btc: json.price_btc,
+    supply: json.supply,
+    total_cap: json.total_cap,
+    volume: json.volume,
+    timestamps: Date.now()
+  })
+
+  newCoinCap.save().then(function(result){
+    console.log('Successful New CoinCap.', result);
+  }).catch(function(err){
+    console.log('Error. CoinCap Not Created.', err);
+  })
+})
+
+fetchUrl('https://poloniex.com/public?command=returnTicker', function(error, meta, result){
+  let json = JSON.parse(result)
+  let LTC = json.BTC_LTC
+
+  let newLTC = new Poloniex({
+    coin: "LTC",
+    pair_name: "BTC_LTC",
+    baseVolume: LTC.baseVolume,
+    high24Hr: LTC.high24Hr,
+    highestBid: LTC.highestBid,
+    lastTradePrice: LTC.last,
+    low24Hr: LTC.low24Hr,
+    lowestAsk: LTC.lowestAsk,
+    percentChange: LTC.percentChange,
+    quoteVolume: LTC.quoteVolume,
+    timestamps: Date.now()
+  })
+
+  newLTC.save().then(function(result){
+    console.log('Successful New Poloniex.', result);
+  }).catch(function(err){
+    console.log('Error. Poloniex Not Created.', err);
+  })
+
+
+  let ETH = json.BTC_ETH
+
+  let newETH = new Poloniex({
+    coin: "ETH",
+    pair_name: "BTC_ETH",
+    baseVolume: ETH.baseVolume,
+    high24Hr: ETH.high24Hr,
+    highestBid: ETH.highestBid,
+    lastTradePrice: ETH.last,
+    low24Hr: ETH.low24Hr,
+    lowestAsk: ETH.lowestAsk,
+    percentChange: ETH.percentChange,
+    quoteVolume: ETH.quoteVolume,
+    timestamps: Date.now()
+  })
+
+  newETH.save().then(function(result){
+    console.log('Successful New Poloniex.', result);
+  }).catch(function(err){
+    console.log('Error. Poloniex Not Created.', err);
+  })
+
+
+  let DASH = json.BTC_DASH
+
+  let newDASH = new Poloniex({
+    coin: "DASH",
+    pair_name: "BTC_DASH",
+    baseVolume: DASH.baseVolume,
+    high24Hr: DASH.high24Hr,
+    highestBid: DASH.highestBid,
+    lastTradePrice: DASH.last,
+    low24Hr: DASH.low24Hr,
+    lowestAsk: DASH.lowestAsk,
+    percentChange: DASH.percentChange,
+    quoteVolume: DASH.quoteVolume,
+    timestamps: Date.now()
+  })
+
+
+  newDASH.save().then(function(result){
+    console.log('Successful New Poloniex.', result);
+  }).catch(function(err){
+    console.log('Error. Poloniex Not Created.', err);
+  })
+})
 
 fetchUrl('https://api.kraken.com/0/public/Ticker?pair=ETHXBT', function(error, meta, result){
   let json = JSON.parse(result)
 
   let ETH = json.result.XETHXXBT
-
-  console.log(ETH);
 
   let newKraken = new Kraken({
     pair_name: "BTC_ETH",
@@ -212,8 +210,6 @@ fetchUrl('https://api.kraken.com/0/public/Ticker?pair=LTCXBT', function(error, m
 
   let LTC = json.result.XLTCXXBT
 
-  console.log(LTC);
-
   let newKraken = new Kraken({
     pair_name: "BTC_LTC",
     ask: {
@@ -260,9 +256,7 @@ fetchUrl('https://api.kraken.com/0/public/Ticker?pair=LTCXBT', function(error, m
 fetchUrl('https://api.kraken.com/0/public/Ticker?pair=DASHXBT', function(error, meta, result){
   let json = JSON.parse(result)
 
-  let DASH = json.result.XDASHXXBT
-
-  console.log(DASH);
+  let DASH = json.result.DASHXBT
 
   let newKraken = new Kraken({
     pair_name: "BTC_DASH",
